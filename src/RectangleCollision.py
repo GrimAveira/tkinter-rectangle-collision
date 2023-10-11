eacbef4c9d34414819c5343b3362f527b0be7d93
GrimAveira <st.tryed@gmail.com>
2023-10-11 11:52:34 +0400

    Formatting and documenting code

--- src/RectangleCollision.py
+++ src/RectangleCollision.py
@@ -1,109 +1,142 @@
 import re
-import tkinter as tk
+from tkinter import Tk, Label, StringVar, Button, Canvas, Menu, Entry
 
 
 class RectangleCollision:
     def __init__(self, width=800, height=300):
 
-        self.width = width
-        self.height = height
-        self.root = tk.Tk()
-        self.root.geometry(f"{self.width}x{self.height}")
-        self.root.title("Rectangle Collision")
+        # window options
+        self.__width = width
+        self.__height = height
+        self.__root = Tk()
+        self.__root.geometry(f"{self.__width}x{self.__height}")
+        self.__root.title("Rectangle Collision")
 
-        self.errmsg = tk.StringVar()
-
-        error_label = tk.Label(
-            foreground="red", textvariable=self.errmsg, wraplength=250)
-        error_label.grid(
+        # error label
+        self.__errmsg = StringVar()
+        error__label = Label(
+            foreground="red", textvariable=self.__errmsg, wraplength=250)
+        error__label.grid(
             column=0, row=5, columnspan=4, padx=10, pady=10)
 
-        self.rec1_mass_label = tk.Label(
-            text="Масса объекта слева", font='Calibri 11')
-        self.rec1_mass_label.grid(column=0, row=0)
-        self.rec1_mass_entry = tk.Entry(width=20)
-        self.rec1_mass_entry.grid(column=0, row=1)
+        # labels and entry for mass and velocity
+        self.__rec1_mass_label = Label(
+            text="Mass of the left object", font='Calibri 11')
+        self.__rec1_mass_label.grid(column=0, row=0)
+        self.__rec1_mass_entry = Entry(width=20)
+        self.__rec1_mass_entry.grid(column=0, row=1)
 
-        self.rec2_mass_label = tk.Label(
-            text="Масса объекта справа", font='Calibri 11')
-        self.rec2_mass_label.grid(column=1, row=0)
-        self.rec2_mass_entry = tk.Entry(width=20)
-        self.rec2_mass_entry.grid(column=1, row=1)
+        self.__rec2_mass_label = Label(
+            text="Mass of the right object", font='Calibri 11')
+        self.__rec2_mass_label.grid(column=1, row=0)
+        self.__rec2_mass_entry = Entry(width=20)
+        self.__rec2_mass_entry.grid(column=1, row=1)
 
-        self.rec1_velocity_label = tk.Label(
-            text="Скорость объекта слева", font='Calibri 11')
-        self.rec1_velocity_label.grid(column=2, row=0)
-        self.rec1_velocity_entry = tk.Entry(width=20)
-        self.rec1_velocity_entry.grid(column=2, row=1)
+        self.__rec1_velocity_label = Label(
+            text="Velocity of the left object", font='Calibri 11')
+        self.__rec1_velocity_label.grid(column=2, row=0)
+        self.__rec1_velocity_entry = Entry(width=20)
+        self.__rec1_velocity_entry.grid(column=2, row=1)
 
-        self.rec2_velocity_label = tk.Label(
-            text="Скорость объекта справа", font='Calibri 11')
-        self.rec2_velocity_label.grid(column=3, row=0)
-        self.rec2_velocity_entry = tk.Entry(width=20)
-        self.rec2_velocity_entry.grid(column=3, row=1)
+        self.__rec2_velocity_label = Label(
+            text="Velocity of the right object", font='Calibri 11')
+        self.__rec2_velocity_label.grid(column=3, row=0)
+        self.__rec2_velocity_entry = Entry(width=20)
+        self.__rec2_velocity_entry.grid(column=3, row=1)
 
-        self._acceptButton = tk.Button(
-            self.root, text="Применить данные и запустить коллизию", command=self.button_create_handler, font='Calibri 12', borderwidth="2", relief="solid")
-        self._acceptButton.grid(
+        # button for start action
+        self.__acceptButton = Button(
+            self.__root, text="Accept data and start collision", command=self.button_create_handler, font='Calibri 12', borderwidth="2", relief="solid")
+        self.__acceptButton.grid(
             column=0, row=4, columnspan=4, padx=10, pady=10)
 
-        self.canvas = tk.Canvas(
-            self.root, width=self.width, height=self.height, bg="white")
-        self.canvas.grid(column=0, row=6, columnspan=4)
-        self.rectangles = []
+        # canvas for drawing
+        self.__canvas = Canvas(
+            self.__root, width=self.__width, height=self.__height, bg="white")
+        self.__canvas.grid(column=0, row=6, columnspan=4)
+        self.__rectangles = []
 
-        self.main_menu = tk.Menu()
-        self.main_menu.add_cascade(label="File")
-        self.main_menu.add_cascade(label="Edit")
-        self.main_menu.add_cascade(label="View")
+        # menu header
+        self.__main_menu = Menu()
+        self.__main_menu.add_cascade(label="help")
+        self.__main_menu.add_cascade(label="version")
 
-        self.root.config(menu=self.main_menu)
+        self.__root.config(menu=self.__main_menu)
 
-        self.root.bind('<Escape>', self.close_app)
+        # bind for close app
+        self.__root.bind('<Escape>', self.__close_app)
 
     def button_create_handler(self):
-        mass1 = self.rec1_mass_entry.get()
-        mass2 = self.rec1_mass_entry.get()
-        vel1 = self.rec1_velocity_entry.get()
-        vel2 = self.rec2_velocity_entry.get()
-        if (self.entry_input_validate(mass1) and self.entry_input_validate(mass2) and self.entry_input_validate(vel1) and self.entry_input_validate(vel2)):
-            self.rectangles = []
-            self.create_rectangle(150, 100, 200, 150, int(mass1), [
-                int(vel1)/100, 0], "aqua")
-            self.create_rectangle(600, 100, 650, 150, int(mass2), [
-                -int(vel2)/100, 0], "black")
-            self.start_collision()
+        """Button to start the collision.
+        """
 
-    def entry_input_validate(self, input_value):
+        mass1 = self.__rec1_mass_entry.get()
+        mass2 = self.__rec1_mass_entry.get()
+        vel1 = self.__rec1_velocity_entry.get()
+        vel2 = self.__rec2_velocity_entry.get()
+        if (self.__entry_input_validate(mass1) and self.__entry_input_validate(mass2) and self.__entry_input_validate(vel1) and self.__entry_input_validate(vel2)):
+            self.__rectangles = []
+            self.__create_rectangle(150, 100, 200, 150, int(mass1), [
+                int(vel1)/100, 0], "aqua")
+            self.__create_rectangle(600, 100, 650, 150, int(mass2), [
+                -int(vel2)/100, 0], "black")
+            self.__start_collision()
+
+    def __entry_input_validate(self, input_value):
+        """Destroy tkinter application.
+
+        Keyword arguments:
+        input_value:str -- the entry argument of the field to be validated
+
+        """
         result = re.match("^[1-9]{1}$", input_value) is not None
         if not result and len(input_value) <= 12:
-            self.errmsg.set(
-                "Каждое поле для ввода должно быть заполнено цифрой от 1 до 9")
+            self.__errmsg.set(
+                "Each input field must be filled with a number from 1 to 9")
         else:
-            self.errmsg.set("")
+            self.__errmsg.set("")
         return result
 
-    def create_rectangle(self, x1, y1, x2, y2, mass, velocity, color):
+    def __create_rectangle(self, x1, y1, x2, y2, mass, velocity, color):
+        """Creates objects that will interact with each other.
+
+        Keyword arguments:
+        x1:int -- the coordinate for the initial rendering of the object
+        y1:int -- the coordinate for the initial rendering of the object
+        x2:int -- the coordinate for the initial rendering of the object
+        y2:int -- the coordinate for the initial rendering of the object
+        mass:int -- reflects the mass of the object
+        velocity:int -- reflects the velocity of the object
+        color:str -- reflects the color of the object
+
+        """
+
         rectangle = {'x1': x1, 'y1': y1, 'x2': x2, 'y2': y2,
                      'mass': mass, 'velocity': velocity, 'color': color}
-        self.rectangles.append(rectangle)
+        self.__rectangles.append(rectangle)
 
-    def update_rectangles(self):
-        for rectangle in self.rectangles:
+    def __update_rectangles(self):
+        """Changes the coordinates of objects depending on the speed.
+        """
+
+        for rectangle in self.__rectangles:
             rectangle['x1'] += rectangle['velocity'][0]
             rectangle['y1'] += rectangle['velocity'][1]
             rectangle['x2'] += rectangle['velocity'][0]
             rectangle['y2'] += rectangle['velocity'][1]
-            if rectangle['x1'] < 0 or rectangle['x2'] > self.width:
+            if rectangle['x1'] < 0 or rectangle['x2'] > self.__width:
                 rectangle['velocity'][0] *= -1
-            if rectangle['y1'] < 0 or rectangle['y2'] > self.height:
+            if rectangle['y1'] < 0 or rectangle['y2'] > self.__height:
                 rectangle['velocity'][1] *= -1
 
-    def check_collisions(self):
-        for i in range(len(self.rectangles)):
-            for j in range(i + 1, len(self.rectangles)):
-                rect1 = self.rectangles[i]
-                rect2 = self.rectangles[j]
+    def __check_collisions(self):
+        """Calculates the collision velocity.
+        """
+
+        for i in range(len(self.__rectangles)):
+            for j in range(i + 1, len(self.__rectangles)):
+                rect1 = self.__rectangles[i]
+                rect2 = self.__rectangles[j]
                 if rect1['x1'] < rect2['x2'] and rect1['x2'] > rect2['x1'] and rect1['y1'] < rect2['y2'] and rect1['y2'] > rect2['y1']:
                     v1 = ((rect1['mass'] - rect2['mass']) * rect1['velocity'][0] + 2 *
                           rect2['mass'] * rect2['velocity'][0]) / (rect1['mass'] + rect2['mass'])
@@ -112,27 +145,33 @@ class RectangleCollision:
                     rect1['velocity'][0] = v1
                     rect2['velocity'][0] = v2
 
-    def draw_rectangles(self):
-        self.canvas.delete("all")
-        for rectangle in self.rectangles:
-            self.canvas.create_rectangle(
-                rectangle['x1'], rectangle['y1'], rectangle['x2'], rectangle['y2'], fill=rectangle['color'], tags="4")
-        self.canvas.update()
+    def __draw_rectangles(self):
+        """Draws objects by coordinates.
+        """
 
-    def start_collision(self):
+        self.__canvas.delete("all")
+        for rectangle in self.__rectangles:
+            self.__canvas.create_rectangle(
+                rectangle['x1'], rectangle['y1'], rectangle['x2'], rectangle['y2'], fill=rectangle['color'], tags="4")
+        self.__canvas.update()
+
+    def __start_collision(self):
+        """Starts rendering and interaction between objects.
+        """
+
         while True:
-            self.update_rectangles()
-            self.check_collisions()
-            self.draw_rectangles()
+            self.__update_rectangles()
+            self.__check_collisions()
+            self.__draw_rectangles()
+
+    def __close_app(self):
+        """Destroy tkinter application.
+        """
+
+        self.__root.destroy()
 
     def start_app(self):
-        self.root.mainloop()
-
-    def close_app(self, _):
-        """Destroy tkinter application.
-
-        Keyword arguments:
-        self -- the mandatory default argument (default self)
-
+        """Start tkinter application.
         """
-        self.root.destroy()
+
+        self.__root.mainloop()
