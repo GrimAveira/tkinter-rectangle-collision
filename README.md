236c1c8c8d08b6fcb827a6a9181706a21a5d9ced
GrimAveira <st.tryed@gmail.com>
2023-10-13 22:07:09 +0400

    Changlelog, Readme and Storage.py fixes Add v0.0.3 CHANGELOG.md Add README.md without gif Add in Storage.py v0.0.3

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# tkinter-rectangle-collision
+# tkinter rectangle-collision
+
+![GitHub Workflow Status (with event)](https://img.shields.io/github/actions/workflow/status/grimaveira/tkinter-resize-inputed-text-app/python-app.yml?label=Build%20and%20tests)
+![GitHub repo size](https://img.shields.io/github/repo-size/grimaveira/tkinter-resize-inputed-text-app)
+![GitHub release (with filter)](https://img.shields.io/github/v/release/grimaveira/tkinter-resize-inputed-text-app)
+
+## Content
+
+- [Preview](#preview)
+
+- [How to install](#how-to-install-and-start)
+
+- [Task 6](#task-6)
+
+## Preview
+
 A Python application with tkinter that allows you to look at the collision of two rectangular bodies at a given mass and velocity
+
+![Watch the video](./AppPreview.gif)
+
+## How to install and start
+
+```bash
+$ git clone https://github.com/GrimAveira/tkinter-resize-inputed-text-app.
+
+$ sudo apt install python3
+(linux)
+
+$ pip install tk
+
+$ pip install pytest
+# pytest for tests
+$ python main.py
+(or python3 on linux)
+
+```
+
+## Task 6
+
+Требуется составить программу, которая выводила бы на экран, начиная с заданной позиции, заданную строку текста, заключив ее в двойную рамку. При каждом нажатии клавиш ‘>‘ размер символов должен увеличиваться в 2 раза, а при нажатии клавиши ‘<‘ - уменьшаться в 2 раза. выход по нажатию клавиши Esc
--- src/Storage.py
+++ src/Storage.py
@@ -4,8 +4,8 @@ class Storage:
 
     def __init__(self):
         self.__about = "A Python application with tkinter that allows you to look at the collision of two rectangular bodies at a given mass and velocity"
-        self.__version = "Version", "v0.0.2\n\nAdded\n\n- Header menu (About, Version)\n- Entry and labels for mass and velocity relevant objects\n- Inputs validation\n- Start button collision\n- Test for function validation\n\nFixed\n\n- Formatting code for standarts\n- Renaming variables for readability"
-        self.__help = ""
+        self.__version = "v0.0.3\n\nAdded\n\n- Working header menu with About, Version and Help\n- Test for function storage\n\n- All program logic is divided into several classes: Storage, Controller, Factory\n\nFixed\n\n- Formatting code for standarts\n- Same mass variable rectangle 1 and rectangle 2\n- RactangleCollision.py Tk parametr constructor\n\n\nRemoved\n\n- RactangleCollision.py unused variables\n\nv0.0.2\n\nAdded\n\n- Header menu (About, Version)\n- Entry and labels for mass and velocity relevant objects\n- Inputs validation\n- Start button collision\n- Test for function validation\n\nFixed\n\n- Formatting code for standarts\n- Renaming variables for readability"
+        self.__help = "It is possible to enter data in 4 fields. The first 2 are responsible for the mass of objects, the second 2 are responsible for speed. To view the interaction of objects, click on the Start button. To exit the program, use Escape"
 
     def get_about(self) -> str:
         """Return About from storage
